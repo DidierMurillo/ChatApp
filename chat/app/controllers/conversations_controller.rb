fecf73c262010a0ee3887b68a57a143beed9acc7
DidierMurillo <didiermurillo@unitec.edu>
2018-02-25 16:15:17 -0600

    Message & Concersation controllers

--- chat/app/controllers/conversations_controller.rb
+++ chat/app/controllers/conversations_controller.rb
@@ -1,8 +1,19 @@
 class ConversationsController < ApplicationController
+  
   def create
     @conversation = Conversation.get(current_user.id, params[:user_id])
     
     add_to_conversations unless conversated?
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def close
+    @conversation = Conversation.find(params[:id])
+
+    session[:conversations].delete(@conversation.id)
 
     respond_to do |format|
       format.js
@@ -19,4 +30,8 @@ class ConversationsController < ApplicationController
   def conversated?
     session[:conversations].include?(@conversation.id)
   end
+
+  
+
+
 end
--- chat/config/routes.rb
+++ chat/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'conversations/create'
-
   root 'home#index'
 
   devise_for :users
 
-  resources :conversations, only: [:create]
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+    resources :messages, only: [:create]	
+   end
+  
 end
